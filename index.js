ba3e91a93b296af827465abb26459673ac619379
Hannah Wenger <hamecow@gmail.com>
2021-12-03 08:53:03 -0500

    Add websocket server connection to index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const path = require("path");
+const ws = require("ws");
 
 const app = express();
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on("connection", (socket) => {
+  socket.on("message", (message) => console.log(message));
+});
+const server = app.listen(9090);
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
+  });
+});
 const port = process.env.PORT || 8080;
 app.use(express.static(__dirname + "/public/"));
 // sendFile will go here
@@ -11,3 +22,9 @@ app.get("/", function (req, res) {
 
 app.listen(port);
 console.log("http://localhost:" + port);
+const client = new ws("ws://localhost:9090");
+
+client.on("open", () => {
+  // Causes the server to print "Hello"
+  client.send("Hello");
+});
